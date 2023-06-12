fbc3a2dc5f9c2acf19b574a8fcce5049c8c8ed9c
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-06-12 18:19:34 +0300

    AlterationArray method has been added

--- Program.cs
+++ Program.cs
@@ -10,4 +10,49 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] CreateArray(int size)
+{
+    string[] arr = new string[size];
 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Please enter the first elemet of the array: ");
+        arr[i] = Convert.ToString(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"\"{arr[i]}\", ");
+        else Console.Write($"\"{arr[i]}\"");
+    }
+    Console.Write("]");
+}
+
+string[] AlterationArray(string[] arr)
+{
+    string[] arrSec = new string[arr.Length];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+        {  
+                if (arr[i].Length < 3) 
+                {
+                    arrSec[j] = arr[i];
+                    j++;
+                }             
+        }
+        return arrSec;
+}
+
+Console.WriteLine("Please enter the size of your array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+string[] array = CreateArray(length);
+PrintArray(array);
+Console.WriteLine("->");
+string[] arraySecond = AlterationArray(array);
+PrintArray(arraySecond);
---
+++ README.md
@@ -0,0 +1,15 @@
+# Description of the program
+
+The program creates a new array of strings from the original array of strings, removing all strings that are equal to or greater than 3 characters.
+
+To create the initial array, the program asks the user to determine the size of the array, and then add all the elements from the keyboard.
+
+This algorithm uses arrays, not collections.
+
+
+## Examples:
+* [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+* [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+* [“Russia”, “Denmark”, “Kazan”] → []
+
+
